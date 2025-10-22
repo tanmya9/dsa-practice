e963b5fb4cb1251ec4a3f2ea5fb7090f0fba54be
Tanmya Srivastava <146061143+tanmya9@users.noreply.github.com>
2025-10-22 15:02:45 +0530

    Implement Node class and print method in LlSwap
    Added a Node class and implemented print method for linked list. Initialized linked list in main method and added swap method placeholder.

---
+++ linkedlist/LlSwap.java
@@ -0,0 +1,53 @@
+class LlSwap
+{
+   public static class Node
+	{
+		int data;
+		Node next;
+		public Node(int data)
+		{
+			this.data=data;
+			this.next=null;
+		}
+	}
+	
+	public static Node head;
+	public static Node tail;
+
+  public void swap(int x, int y)
+  {
+    
+  }
+
+  public void print()
+	{
+		if(head==null)
+		{
+			System.out.println("LinkedList is empty");
+			return;
+		}
+		Node temp=head;
+		while(temp!=null)
+		{
+			System.out.print(temp.data+" ");
+			temp=temp.next;
+		}
+		System.out.println();
+	}
+
+  public static void main(String args[])
+	{
+		LinkedListSwap ll=new LinkedListSwap();
+		ll.head=new Node(1);
+		ll.head.next=new Node(2);
+		ll.head.next.next=new Node(3);
+		ll.head.next.next.next=new Node(4);
+		ll.head.next.next.next.next=new Node(5);
+		ll.head.next.next.next.next.next=new Node(6);
+		ll.head.next.next.next.next.next.next=new Node(7);
+		ll.print();
+		
+		ll.swap(3, 5);
+		ll.print();
+	}
+}
