d3128a005266b5d9beef1ee46a6bb9ba822aaa86
Tanmya Srivastava <146061143+tanmya9@users.noreply.github.com>
2025-10-27 10:43:47 +0530

    Add OddEven class for linked list manipulation

---
+++ linkedlist/OddEven.java
@@ -0,0 +1,29 @@
+class OddEven
+{
+  public static Node
+  {
+    int data;
+    Node next;
+    public Node(int data)
+    {
+      this.data=data;
+      this.next=null;
+    }
+  }
+  public static Node head;
+  public static Node tail;
+
+public Node OddEven(Node head)
+{
+  Node resStart=nulll
+  Node resEnd=null;
+
+  Node curr=head;
+  Node prev=null;
+
+  while(curr!=null)
+  {
+    Node nextnode=prev.next;
+      
+  }
+}
