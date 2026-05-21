c69dfa4b5fdf12ccf64b070ffa169ca2a7268ec0
Tanmya Srivastava <tanmyasrivastava25@gmail.com>
2026-05-21 16:19:10 +0530

    Updated Using2stack file

--- Queues/Using2stacks.java
+++ Queues/Using2stacks.java
@@ -1,5 +1,95 @@
+//package Queues;
+//import java.util.*;
+//public class Using2stacks {
+//	public static class Queue
+//	{
+//		static Stack<Integer> s1=new Stack<>();
+//		static Stack<Integer> s2=new Stack<>();
+//		
+//		//checking is stack is empty
+//		public static boolean isEmpty()
+//		{
+//			return s1.isEmpty();
+//		}
+//		
+//		//add
+//		public static void add(int data)
+//		{
+//			while(!s1.isEmpty())
+//			{
+//				s2.push(s1.pop());
+//			}
+//			s1.pop();
+//			while(!s2.isEmpty()) {
+//				s1.push(s2.pop());
+//			}
+//		}
+//		
+//		//remove
+//		public static int remove()
+//		{
+//			if(isEmpty())
+//			{
+//				return -1;
+//			}
+//			
+//			return s1.pop();
+//		}
+//		public static void main(String args[])
+//		{
+//			Queue q=new Queue();
+//		}
+//	}
+//}
+
 package Queues;
-
-public class Using2stacks {
-
+import java.util.*;
+public class Using2stacks
+{
+	static class Queue
+	{
+		static Stack<Integer> s1=new Stack<>();
+		static Stack<Integer> s2=new Stack<>();
+		
+		//check if empty
+		public static Boolean isEmpty()
+		{
+			return s1.isEmpty();
+		}
+		
+		//add elements in s1
+		public static void add(int data)
+		{
+			while(!s1.isEmpty())
+			{
+				s2.push(s1.pop());
+			}
+			s1.push(data);
+			while(!s2.isEmpty()) {
+				s1.push(s2.pop());
+			}
+			//removing
+		}
+		public static int remove()
+		{
+			if(isEmpty())
+			{
+				return -1;
+			}
+			
+			return s1.pop();
+		}
+	}
+	public static void main(String args[])
+	{
+		Queue q=new Queue();
+		q.add(1);
+		q.add(2);
+		q.add(3);
+		
+		while(!q.isEmpty())
+		{
+			System.out.println(q.remove());
+		}
+	}
 }
