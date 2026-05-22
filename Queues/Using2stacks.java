7390770c03c6d6dc6252c910d30c6ca602636f21
Tanmya Srivastava <tanmyasrivastava25@gmail.com>
2026-05-22 17:40:41 +0530

    Added Using2queues

--- Queues/Using2stacks.java
+++ Queues/Using2stacks.java
@@ -42,6 +42,59 @@
 //	}
 //}
 
+//package Queues;
+//import java.util.*;
+//public class Using2stacks
+//{
+//	static class Queue
+//	{
+//		static Stack<Integer> s1=new Stack<>();
+//		static Stack<Integer> s2=new Stack<>();
+//		
+//		//check if empty
+//		public static Boolean isEmpty()
+//		{
+//			return s1.isEmpty();
+//		}
+//		
+//		//add elements in s1
+//		public static void add(int data)
+//		{
+//			while(!s1.isEmpty())
+//			{
+//				s2.push(s1.pop());
+//			}
+//			s1.push(data);
+//			while(!s2.isEmpty()) {
+//				s1.push(s2.pop());
+//			}
+//			
+//		}
+		//removing
+//		public static int remove()
+//		{
+//			if(isEmpty())
+//			{
+//				return -1;
+//			}
+//			
+//			return s1.pop();
+//		}
+//	}
+//	public static void main(String args[])
+//	{
+//		Queue q=new Queue();
+//		q.add(1);
+//		q.add(2);
+//		q.add(3);
+//		
+//		while(!q.isEmpty())
+//		{
+//			System.out.println(q.remove());
+//		}
+//	}
+//}
+
 package Queues;
 import java.util.*;
 public class Using2stacks
@@ -51,33 +104,41 @@ public class Using2stacks
 		static Stack<Integer> s1=new Stack<>();
 		static Stack<Integer> s2=new Stack<>();
 		
-		//check if empty
+		//checking is stack is empty
 		public static Boolean isEmpty()
 		{
-			return s1.isEmpty();
+			return s1.isEmpty();		
 		}
 		
-		//add elements in s1
+		//add
 		public static void add(int data)
 		{
-			while(!s1.isEmpty())
-			{
+			while(!s1.isEmpty()) {
 				s2.push(s1.pop());
 			}
 			s1.push(data);
 			while(!s2.isEmpty()) {
 				s1.push(s2.pop());
 			}
-			//removing
 		}
+		
+		//remove
 		public static int remove()
 		{
 			if(isEmpty())
 			{
 				return -1;
 			}
-			
 			return s1.pop();
+		}
+		
+		public static int peek()
+		{
+			if(isEmpty())
+			{
+				return -1;
+			}
+			return s1.peek();
 		}
 	}
 	public static void main(String args[])
@@ -86,6 +147,7 @@ public class Using2stacks
 		q.add(1);
 		q.add(2);
 		q.add(3);
+		q.add(4);
 		
 		while(!q.isEmpty())
 		{
