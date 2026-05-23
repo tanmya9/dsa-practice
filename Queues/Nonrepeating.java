3196e94edee46baed64e6bd23562fcc452590a93
Tanmya Srivastava <tanmyasrivastava25@gmail.com>
2026-05-23 12:01:08 +0530

    Added non repeating characters file

--- Queues/Nonrepeating.java
+++ Queues/Nonrepeating.java
@@ -1,5 +1,39 @@
 package Queues;
-
+import java.util.*;
 public class Nonrepeating {
-
+	public static void printNonRepeating(String str)
+	{
+		int freq[]=new int[26]; // to track frequency from a-z
+		Queue<Character> q=new LinkedList<>();
+		
+		for(int i=0;i<str.length();i++)
+		{
+			char ch=str.charAt(i);
+			q.add(ch); //adding it in the queue
+			freq[ch-'a']++; //any char-'a' will give the index of that char
+			
+			//checking queue from THE FRONT if 1st non repeating element exists
+			while(!q.isEmpty() && freq[q.peek()-'a']>1)
+			{
+				q.remove();
+			} //if this loop ends then there can be 2 cases:
+			// queue has become empty
+			//freq of the character has become greater than 1
+			
+			if(q.isEmpty())
+			{
+				System.out.print(-1+" ");
+			}
+			else
+			{
+				System.out.print(q.peek()+" ");
+			}
+		}
+		System.out.println();
+	}
+	public static void main(String args[])
+	{
+		String str="aabccxb";
+		printNonRepeating(str);
+	}
 }
