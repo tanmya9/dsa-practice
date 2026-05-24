5351c2266afe1acdfe7af9eb1e158967bba2cec8
Tanmya Srivastava <tanmyasrivastava25@gmail.com>
2026-05-25 02:33:02 +0530

    Added Reversal file

--- Queues/Interleave.java
+++ Queues/Interleave.java
@@ -1,19 +1,58 @@
+//package Queues;
+//import java.util.*;
+//public class Interleave {
+//	public static void interLeave(Queue<Integer>q)
+//	{
+//		Queue<Integer> firstHalf=new LinkedList<>(); //2nd queue to store 1st half of the queue
+//		int size=q.size();
+//		
+//		for(int i=0;i<size/2;i++)
+//		{
+//			firstHalf.add(q.remove()); //1st half elements are removed from original queue first and then get added to the 2nd queue
+//		}
+//		while(!firstHalf.isEmpty())
+//		{
+//			q.add(firstHalf.remove()); //now from the 1st half, elements are removed and added to the 2nd half of the original queue
+//			q.add(q.remove()); //elements in original queue from front are added to the rear of queue
+//		}
+//	}
+//	public static void main(String args[])
+//	{
+//		Queue<Integer> q=new LinkedList<>();
+//		q.add(1);
+//		q.add(2);
+//		q.add(3);
+//		q.add(4);
+//		q.add(5);
+//		q.add(6);
+//		q.add(7);
+//		q.add(8);
+//		q.add(9);
+//		q.add(10);
+//		interLeave(q);
+//		while(!q.isEmpty()) {
+//			System.out.println(q.remove()+" ");
+//		}
+//		System.out.println();
+//	}
+//}
+
 package Queues;
 import java.util.*;
-public class Interleave {
-	public static void interLeave(Queue<Integer>q)
+public class Interleave
+{
+	public static void interLeave(Queue<Integer> q)
 	{
-		Queue<Integer> firstHalf=new LinkedList<>(); //2nd queue to store 1st half of the queue
+		Queue<Integer> firstHalf=new LinkedList<>();
 		int size=q.size();
 		
 		for(int i=0;i<size/2;i++)
 		{
-			firstHalf.add(q.remove()); //1st half elements are removed from original queue first and then get added to the 2nd queue
+			firstHalf.add(q.remove());
 		}
-		while(!firstHalf.isEmpty())
-		{
-			q.add(firstHalf.remove()); //now from the 1st half, elements are removed and added to the 2nd half of the original queue
-			q.add(q.remove()); //elements in original queue from front are added to the rear of queue
+		while(!firstHalf.isEmpty()) {
+			q.add(firstHalf.remove());
+			q.add(q.remove());
 		}
 	}
 	public static void main(String args[])
@@ -25,13 +64,9 @@ public class Interleave {
 		q.add(4);
 		q.add(5);
 		q.add(6);
-		q.add(7);
-		q.add(8);
-		q.add(9);
-		q.add(10);
 		interLeave(q);
 		while(!q.isEmpty()) {
-			System.out.println(q.remove()+" ");
+			System.out.print(q.remove()+" ");
 		}
 		System.out.println();
 	}
---
+++ Queues/Reversal.java
@@ -0,0 +1,61 @@
+package Queues;
+import java.util.*;
+public class Reversal {
+	public static void reverse(Queue<Integer>q)
+	{
+		Stack<Integer> s=new Stack<>();
+		while(!q.isEmpty())
+		{
+			s.push(q.remove());
+		}
+		while(!s.isEmpty()) {
+			q.add(s.pop());
+		}
+	}
+	public static void main(String args[])
+	{
+		Queue<Integer> q=new LinkedList<>();
+		q.add(1);
+		q.add(2);
+		q.add(3);
+		q.add(4);
+		q.add(5);
+		reverse(q);
+		
+		while(!q.isEmpty())
+		{
+			System.out.print(q.remove());
+		}
+		System.out.println();
+	}
+}
+
+//OR
+
+//package Queues;
+//import java.util.*;
+//public class Reversal {
+//	public static void reverse(Queue<Integer>q)
+//	{
+//		Stack<Integer> s=new Stack<>();
+//		while(!q.isEmpty())
+//		{
+//			s.push(q.remove());
+//		}
+//		while(!s.isEmpty()) {
+//			System.out.print(s.pop()); //removing elements from the stack and printing them directly and not storing in the queue again.
+//		}
+//		System.out.println();
+//	}
+//	public static void main(String args[])
+//	{
+//		Queue<Integer> q=new LinkedList<>();
+//		q.add(1);
+//		q.add(2);
+//		q.add(3);
+//		q.add(4);
+//		q.add(5);
+//		reverse(q);
+//	}
+//}
+
