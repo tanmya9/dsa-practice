a713bfac66d4bf0342adb9550efe066c267d5499
Tanmya Srivastava <tanmyasrivastava25@gmail.com>
2026-05-26 10:53:08 +0530

    Updated Using2queues file

--- Queues/Using2stacks.java
+++ Queues/Using2stacks.java
@@ -95,6 +95,67 @@
 //	}
 //}
 
+//package Queues;
+//import java.util.*;
+//public class Using2stacks
+//{
+//	static class Queue
+//	{
+//		static Stack<Integer> s1=new Stack<>();
+//		static Stack<Integer> s2=new Stack<>();
+//		
+//		//checking is stack is empty
+//		public static Boolean isEmpty()
+//		{
+//			return s1.isEmpty();		
+//		}
+//		
+//		//add
+//		public static void add(int data)
+//		{
+//			while(!s1.isEmpty()) {
+//				s2.push(s1.pop());
+//			}
+//			s1.push(data);
+//			while(!s2.isEmpty()) {
+//				s1.push(s2.pop());
+//			}
+//		}
+//		
+//		//remove
+//		public static int remove()
+//		{
+//			if(isEmpty())
+//			{
+//				return -1;
+//			}
+//			return s1.pop();
+//		}
+//		
+//		public static int peek()
+//		{
+//			if(isEmpty())
+//			{
+//				return -1;
+//			}
+//			return s1.peek();
+//		}
+//	}
+//	public static void main(String args[])
+//	{
+//		Queue q=new Queue();
+//		q.add(1);
+//		q.add(2);
+//		q.add(3);
+//		q.add(4);
+//		
+//		while(!q.isEmpty())
+//		{
+//			System.out.println(q.remove());
+//		}
+//	}
+//}
+
 package Queues;
 import java.util.*;
 public class Using2stacks
@@ -107,51 +168,33 @@ public class Using2stacks
 		//checking is stack is empty
 		public static Boolean isEmpty()
 		{
-			return s1.isEmpty();		
+			return s1.isEmpty() && s2.isEmpty();		
 		}
 		
 		//add
 		public static void add(int data)
 		{
-			while(!s1.isEmpty()) {
-				s2.push(s1.pop());
-			}
 			s1.push(data);
-			while(!s2.isEmpty()) {
-				s1.push(s2.pop());
-			}
 		}
 		
-		//remove
 		public static int remove()
 		{
-			if(isEmpty())
-			{
-				return -1;
-			}
-			return s1.pop();
-		}
-		
-		public static int peek()
-		{
-			if(isEmpty())
-			{
-				return -1;
-			}
-			return s1.peek();
+				while(!s1.isEmpty()) {
+					s2.push(s1.pop());
+				}
+			return s2.pop();
 		}
 	}
 	public static void main(String args[])
 	{
 		Queue q=new Queue();
+		q.add(0);
 		q.add(1);
 		q.add(2);
-		q.add(3);
-		q.add(4);
-		
 		while(!q.isEmpty())
 		{
 			System.out.println(q.remove());
 		}
+		
 	}
 }
