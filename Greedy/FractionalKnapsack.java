f1fa8a672797f7f3c5da079f526641cc27e6dd4c
Tanmya Srivastava <tanmyasrivastava25@gmail.com>
2026-06-01 01:17:32 +0530

    Added Minimum absolute Difference

--- Greedy/FractionalKnapsack.java
+++ Greedy/FractionalKnapsack.java
@@ -1,39 +1,74 @@
 package Greedy;
 import java.util.*;
 public class FractionalKnapsack {
+//	public static void main(String args[])
+//	{
+//		int val[]={60,100,120};
+//		int weight[]= {10,20,30};
+//		int W = 50;
+//		
+//		//ratios should be sorted
+//		//creating a 2D array to sort the ratios
+//		//0th col=> index
+//		//1st col=>ratio
+//		
+//		double ratio[][]=new double[val.length][2];
+//		for(int i=0;i<val.length;i++)
+//		{
+//			ratio[i][0]=i;
+//			ratio[i][1]= val[i]/(double)weight[i];
+//		}
+//		//sorting in ascending order
+//		Arrays.sort(ratio, Comparator.comparingDouble(o -> o[1]));
+//		
+//		int capacity = W;
+//		int finalVal=0;
+//		for(int i=ratio.length-1;i>=0;i--) //because we want ratios to be in descending order
+//		{ //jitna bada ratio utna zada value
+//			int idx=(int)ratio[i][0]; //storing index of a particular ratio
+//			if(capacity>=weight[idx])
+//			{
+//				finalVal+=val[idx];
+//				capacity-=weight[idx];
+//			}
+//			else //including fractional item
+//			{
+//				finalVal+=(ratio[i][1] * capacity);
+//				capacity=0;
+//				break;
+//			}
+//		}
+//		System.out.println("Final Value "+finalVal);
+//	}
+	
 	public static void main(String args[])
 	{
-		int val[]={60,100,120};
+		int val[]= {60,100,120};
 		int weight[]= {10,20,30};
-		int W = 50;
-		
-		//ratios should be sorted
-		//creating a 2D array to sort the ratios
-		//0th col=> index
-		//1st col=>ratio
+		int W=50;
 		double ratio[][]=new double[val.length][2];
 		
 		for(int i=0;i<val.length;i++)
 		{
 			ratio[i][0]=i;
-			ratio[i][1]= val[i]/(double)weight[i];
+			ratio[i][1]=val[i]/weight[i];
 		}
-		//sorting in ascending order
 		Arrays.sort(ratio, Comparator.comparingDouble(o -> o[1]));
 		
-		int capacity = W;
 		int finalVal=0;
-		for(int i=ratio.length-1;i>=0;i--) //because we want ratios to be in descending order
-		{ //jitna bada ration utna zada value
-			int idx=(int)ratio[i][0]; //storing index of a particular ratio
+		int capacity=W;
+		
+		for(int i=val.length-1;i>=0;i--)
+		{
+			int idx=(int)ratio[i][0];
 			if(capacity>=weight[idx])
 			{
 				finalVal+=val[idx];
 				capacity-=weight[idx];
 			}
-			else //including fractional item
+			else
 			{
-				finalVal+=(ratio[i][1] * capacity);
+				finalVal+=(ratio[i][1]*capacity);
 				capacity=0;
 				break;
 			}
